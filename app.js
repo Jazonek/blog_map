d804f98613470d3bd8e4c61b2f1eb2d5005836f2
Jazonek <wojciech.jazo@gmail.com>
2019-07-05 18:14:20 +0200

    Changed secure true!

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: false,
+      secure: true,
       maxAge: 60 * 60 * 60 * 100,
       sameSite: true
     }
